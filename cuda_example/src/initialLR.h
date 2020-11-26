9939db06f9418caba8a5abdb5a92d5c576feef24
AdrianHsu <adrianhsu1995@gmail.com>
2020-11-26 08:23:16 +0800

    [UPDATED] Thu 26 Nov 2020 08:23:16 AM CST :bulb:

---
+++ cuda_example/src/initialLR.h
@@ -0,0 +1,8 @@
+#ifndef SERIAL_INITIALLR_H
+#define SERIAL_INITIALLR_H
+
+#include <cstdlib>
+
+void initialLR(double *&L, double *&R, int &numberOfUsers, int &numberOfItems, int &numberOfFeatures);
+
+#endif //SERIAL_INITIALLR_H
